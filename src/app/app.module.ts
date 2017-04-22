64d1516b18ef049d6ba8ebee5a424bcf82cac808
e.zinchenko <idc.zinchenko@gmail.com>
2017-04-22 18:44:25 +0300

    верстка

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { RegionFilterComponent } from './region-filter/region-filter.component';
+import { RegionListComponent } from './region-list/region-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegionFilterComponent,
+    RegionListComponent
   ],
   imports: [
     BrowserModule,
